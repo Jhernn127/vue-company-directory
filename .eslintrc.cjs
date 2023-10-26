13e731eadc8d7a0d952923ba958f980b7817aaa3
Jhernn127 <javi.hdz1426@gmail.com>
2023-10-26 15:13:12 -0500

    Better practices applied , corrected lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
     'prettier/prettier': ['error'],
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
+    
   },
   env: {
     browser: true,
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+/** @type {import(import'vite').userconfig}*/
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
